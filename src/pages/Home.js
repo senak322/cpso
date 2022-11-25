9b4e9a1f9845050032144457ba8d0797a8043925
Alexander Vizniy <senak98@yandex.ru>
2022-11-25 17:00:50 +0300

    add sidebar link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,13 @@
 import React from "react";
-import personImg from "../images/person2.png";
 import Settings from "./Settings.js";
-import { Route, Switch, NavLink, Redirect, useHistory } from "react-router-dom";
+import { Route, Switch, NavLink, Redirect } from "react-router-dom";
 import ProtectedRoute from "../components/ProtectedRoute.js";
 import { FaBars } from "react-icons/fa";
 import { AiFillHome, AiFillSetting } from "react-icons/ai";
-import {CurrentUserContext } from '../contexts/CurrentUserContext.js'
+import UserInfo from "./UserInfo.js";
 
 function Home(props) {
-
-  const userContext = React.useContext(CurrentUserContext)
+  
 
   return (
     <>
@@ -20,59 +18,47 @@ function Home(props) {
         <ul className="side-bar__nav">
           <li className="side-bar__container">
             <NavLink
-              to="/home"
+              to="/home/user-info"
               key="home"
               className="link"
-              activeClassName="link__active"
+              activeClassName="side-bar__active"
             >
               <AiFillHome className="side-bar__icon" />
             </NavLink>
           </li>
           <li className="side-bar__container">
             <NavLink
-              to="/settings"
+              to="/home/settings"
               key="settings"
               className="link"
-              activeClassName="link__active"
+              activeClassName="side-bar__active"
             >
               <AiFillSetting className="side-bar__icon" />
             </NavLink>
           </li>
         </ul>
       </nav>
+
       <Switch>
         <ProtectedRoute
-          path="/settings"
+          path="/home/user-info"
+          component={UserInfo}
+          loggedIn={props.loggedIn}
+          students={props.students}
+        />
+        <ProtectedRoute
+          path="/home/settings"
           component={Settings}
           loggedIn={props.loggedIn}
         />
+        <Route path="/home">
+          {props.loggedIn ? (
+            <Redirect to="/home/user-info" />
+          ) : (
+            <Redirect to="/signin" />
+          )}
+        </Route>
       </Switch>
-      <section className="home">
-        <div className="home__wrapper">
-          <img
-            className="home__user-image"
-            src={personImg}
-            alt="user"
-          ></img>
-          <h1 className="home__user-name">{userContext.name}</h1>
-        </div>
-        <div className="home__wrapper home__container">
-          <h2 className="home__title">Выберете ученика:</h2>
-        </div>
-        <div className="home__wrapper home__container">
-          <button type="button" className="home__add-student">
-            + Добавить ученика
-          </button>
-          <ul className="home__students">
-            {props.students.isArray ? props.students.map((el) => {
-              return (
-              <li className="home__student" key={el.userid}>
-                {el.Student_Name}
-              </li>)
-            }) : props.students}
-          </ul>
-        </div>
-      </section>
     </>
   );
 }
--- src/pages/UserInfo.js
+++ src/pages/UserInfo.js
@@ -1,13 +1,39 @@
 import React from 'react';
+import personImg from "../images/person2.png";
+import {CurrentUserContext } from '../contexts/CurrentUserContext.js'
 
-function Settings() {
+function UserInfo(props) {
+
+    const userContext = React.useContext(CurrentUserContext);
+
     return (
-        <div>
-            <h2>
-                Настройки пользователя
-            </h2>
+        <section className="home">
+        <div className="home__wrapper">
+          <img
+            className="home__user-image"
+            src={personImg}
+            alt="user"
+          ></img>
+          <h1 className="home__user-name">{userContext.name}</h1>
         </div>
+        <div className="home__wrapper home__container">
+          <h2 className="home__title">Выберете ученика:</h2>
+        </div>
+        <div className="home__wrapper home__container">
+          <button type="button" className="home__add-student">
+            + Добавить ученика
+          </button>
+          <ul className="home__students">
+            {props.students.isArray ? props.students.map((el) => {
+              return (
+              <li className="home__student" key={el.userid}>
+                {el.Student_Name}
+              </li>)
+            }) : props.students}
+          </ul>
+        </div>
+      </section>
     )
 }
 
-export default Settings;
+export default UserInfo;
