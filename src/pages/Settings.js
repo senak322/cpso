2e284ce274b1d3557655073d859a9fee4d306a18
Alexander Vizniy <senak98@yandex.ru>
2022-11-29 17:02:17 +0300

    add notifications2

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,13 +1,48 @@
-import React from 'react';
+import React from "react";
+import personImg from "../images/person2.png";
+import { CurrentUserContext } from "../contexts/CurrentUserContext.js";
 
 function Settings() {
-    return (
-        <div>
-            <h2>
-                Настройки пользователя
-            </h2>
-        </div>
-    )
+  const userContext = React.useContext(CurrentUserContext);
+
+  function handleClick() {
+
+  }
+
+  return (
+    <section className="home">
+      <div className="home__wrapper">
+        <img className="home__user-image" src={personImg} alt="user"></img>
+        <h1 className="home__user-name">{userContext.name}</h1>
+      </div>
+      <div className="home__wrapper home__container">
+        <h2 className="home__title">Изменить профиль:</h2>
+      </div>
+      <div className="home__wrapper home__container">
+        <button
+          type="button"
+          className="home__add-student"
+          onClick={handleClick}
+        >
+          Изменить пароль
+        </button>
+        <button
+          type="button"
+          className="home__add-student"
+          onClick={handleClick}
+        >
+          Изменить ФИО
+        </button>
+        <button
+          type="button"
+          className="home__add-student"
+          onClick={handleClick}
+        >
+          Изменить фото профиля
+        </button>
+      </div>
+    </section>
+  );
 }
 
 export default Settings;
