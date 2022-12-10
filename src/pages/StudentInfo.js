5f81578debbb6512817da964d613199756602e20
Alexander Vizniy <senak98@yandex.ru>
2022-12-10 20:37:16 +0300

    add student info

--- src/pages/StudentInfo.js
+++ src/pages/StudentInfo.js
@@ -9,12 +9,14 @@ function StudentInfo({ currentStudent }) {
       <section className="home">
       <div className="home__wrapper">
         <img className="home__user-image" src={personImg} alt="user"></img>
-        <h1 className="home__user-name">{currentStudent.firstname + " " + currentStudent.lastname + " " + currentStudent.middlename}</h1>
+        <h1 className="home__user-name">{currentStudent.lastname + " " + currentStudent.firstname + " " + currentStudent.middlename}</h1>
       </div>
       <div className="home__wrapper home__container">
         <h2 className="home__title home__title_type_student">Информация об ученике</h2>
+        <p className="home__description">Школа: {currentStudent.institution}</p>
         <p className="home__description">E-mail: {currentStudent.username}</p>
-        <p className="home__description">Класс: {currentStudent.department}</p>
+        <p className="home__description">Классы: {currentStudent.department}</p>
+        <p className="home__description">Текущий класс: {currentStudent.current_class}</p>
         <p className="home__description">Оценки: </p>
         
       </div>
