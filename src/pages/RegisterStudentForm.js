deefe0db0d63d506e429cac47bb4595d98d31154
Aleksandr Viznyi <senak98@yandex.ru>
2023-02-07 20:13:03 +0400

    add new styles form

--- src/pages/RegisterStudentForm.js
+++ src/pages/RegisterStudentForm.js
@@ -8,31 +8,48 @@ function RegisterStudentForm() {
       <div className="register__container">
         <h2 className="register__title">Анкета ученика</h2>
         <form className="regiser__form">
-          <label>
-            Фамилия, Имя, Отчество ученика* (поля с звездочкой обязательны для
-            заполнения)
+          <label className="regiser__label">
+            Фамилия, Имя, Отчество ученика
+            <span className="regiser__star">*</span> (поля с звездочкой
+            обязательны для заполнения)
+            <input className="regiser__input" type="text"></input>
           </label>
-          <input></input>
 
-          <label>
+          <label className="regiser__label">
             Укажите выделенный e-mail для создания Личного кабинета ученика на
-            платформе ЦПСО*
+            платформе ЦПСО<span className="regiser__star">*</span>
+            <p className="regiser__text">
+              <span style={{ fontWeight: 700 }}>ВАЖНО!</span> E-mail ученика не
+              должен совпадать с e-mail родителя/куратора. Если Вы уже
+              регистрировали этого ученика, пожалуйста, используйте тот же
+              email, который использовали ранее.
+            </p>
+            <input className="regiser__input" type="email"></input>
           </label>
-          <p>
-            ВАЖНО! E-mail ученика не должен совпадать с e-mail
-            родителя/куратора. Если Вы уже регистрировали этого ученика,
-            пожалуйста, используйте тот же email, который использовали ранее.
+
+          <div className="register__container register__container_type_radio">
+            <p className="regiser__label">
+              Где проходили последнюю аттестацию?
+              <span className="regiser__star">*</span>
+            </p>
+            <label>
+              <input className="regiser__radio" id="cpso" value="cpso" name="school" type="radio"></input> 
+              ЦПСО
+            </label>
+            <label>
+              <input className="regiser__radio" id="not-cpso" value="not-cpso" name="school" type="radio"></input>
+              Не в ЦПСО
+            </label>
+            <label>
+              <input className="regiser__radio" id="not-all" value="not-all" name="school" type="radio"></input>
+              Не проходил совсем
+            </label>
+          </div>
+
+          <p className="regiser__label">
+            Страна постоянного проживания ученика
+            <span className="regiser__star">*</span>
           </p>
-          <input></input>
-
-          <label>
-            Где проходили последнюю аттестацию?*
-            <input type="radio"></input>
-            <input type="radio"></input>
-            <input type="radio"></input>
-          </label>
-
-          <p>Страна постоянного проживания ученика*</p>
           <select>
             <option>1</option>
             <option>2</option>
@@ -40,7 +57,10 @@ function RegisterStudentForm() {
             <option>4</option>
           </select>
 
-          <p>Регион постоянного проживания (только для РФ)*</p>
+          <p>
+            Регион постоянного проживания (только для РФ)
+            <span className="regiser__star">*</span>
+          </p>
           <select>
             <option>1</option>
             <option>2</option>
