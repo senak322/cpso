edfef59af838f8406eff3a6b89138ab6ab94c8be
Aleksandr Viznyi <senak98@yandex.ru>
2023-03-07 20:06:30 +0400

    add classes

---
+++ src/components/Class.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Class({ num }) {
+    return (
+        <li className="home__class">
+            {`${num} класс`}
+        </li>
+    )
+}
+
+export default Class;
--- src/pages/UserInfo.js
+++ src/pages/UserInfo.js
@@ -3,14 +3,19 @@ import personImg from "../images/person2.png";
 import { CurrentUserContext } from "../contexts/CurrentUserContext.js";
 import { Link } from "react-router-dom";
 import Student from "../components/Student.js";
+import Class from "../components/Class.js";
 
-function UserInfo({ students, onOpenAddStudents, onOpenDelete, onChangeStudent }) {
+function UserInfo({
+  students,
+  onOpenAddStudents,
+  onOpenDelete,
+  onChangeStudent,
+}) {
   const userContext = React.useContext(CurrentUserContext);
 
   function handleAddClick() {
     onOpenAddStudents();
   }
-
 
   return (
     <section className="home">
@@ -18,10 +23,9 @@ function UserInfo({ students, onOpenAddStudents, onOpenDelete, onChangeStudent }
         <img className="home__user-image" src={personImg} alt="user"></img>
         <h1 className="home__user-name">{userContext.name}</h1>
       </div>
+
       <div className="home__wrapper home__container">
-        <h2 className="home__title">Выберите ученика:</h2>
-      </div>
-      <div className="home__wrapper home__container">
+        <h2 className="home__title">Мои ученики:</h2>
         <button
           type="button"
           className="home__add-student"
@@ -32,18 +36,30 @@ function UserInfo({ students, onOpenAddStudents, onOpenDelete, onChangeStudent }
         <ul className="home__students">
           {Array.isArray(students)
             ? students.map((el) => {
-
                 return (
                   <Link
                     className="home__link"
                     key={el.id}
                     to={`/home/student${el.id}`}
                   >
-                    <Student el={el} onOpenDelete={onOpenDelete} onChangeStudent={onChangeStudent}/>
+                    <Student
+                      el={el}
+                      onOpenDelete={onOpenDelete}
+                      onChangeStudent={onChangeStudent}
+                    />
                   </Link>
                 );
               })
             : "Список учеников пуст"}
+        </ul>
+      </div>
+      <div className="home__wrapper home__container">
+        <h2 className="home__title">Мои классы:</h2>
+        <ul className="home__students home__students_type_class">
+          Вы приобрели
+          <Class num="6" />
+          <Class num="7" />
+          <Class num="8" />
         </ul>
       </div>
     </section>
