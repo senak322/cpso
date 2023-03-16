8e4ea9d41e94dbed8cfedfc37a53b793b4cee2d1
Aleksandr Viznyi <senak98@yandex.ru>
2023-03-17 00:01:08 +0400

    add docs page

--- src/pages/AddStudentDocs.js
+++ src/pages/AddStudentDocs.js
@@ -20,23 +20,58 @@ function AddStudentDocs() {
     <section className="register">
       <div className="register__container">
         <h1 className="register__add-header">{id} класс</h1>
-        <form className="register__form" onSubmit={handleSubmitForm}></form>
-        <p>Прикрепите подписанные документы</p>
-        <div className="register__docs">
-          <label className="register__label" htmlFor="application">
-            Заявление в школу
-          </label>
-          <input
-            required
-            multiple
-            accept="image/*, .png, .jpg, .jpeg, .pdf"
-            name="application"
-            id="application"
-            type="file"
-            onChange={hadleChangeFiles}
-          ></input>
+        <form className="register__form" onSubmit={handleSubmitForm}>
+          <h4
+            className="register__add-description mb-3"
+            style={{ textAlign: "center" }}
+          >
+            Прикрепите подписанные документы
+          </h4>
+          <div className="register__docs">
+            <label className="register__label" htmlFor="application">
+              Заявление в школу
+              <p className="register__comment">
+                Просим прикрепить качественную копию заполненного документа в
+                формате PDF, а не фото с телефона. Если у вас несколько страниц
+                скан-копии отдельными файлами, то вы можете прикрепить их все,
+                выбрав сразу несколько файлов.
+              </p>
+            </label>
+            <input
+              required
+              multiple
+              accept="image/*, .png, .jpg, .jpeg, .pdf"
+              name="application"
+              id="application"
+              type="file"
+              onChange={hadleChangeFiles}
+            ></input>
+          </div>
+          <div className="register__docs">
+            <label className="register__label" htmlFor="opd-to-class">
+              Согласие на обработку персональных данных
+              <p className="register__comment">
+                Просим прикрепить качественную копию заполненного документа в
+                формате PDF, а не фото с телефона. Если у вас несколько страниц
+                скан-копии отдельными файлами, то вы можете прикрепить их все,
+                выбрав сразу несколько файлов.
+              </p>
+            </label>
+            <input
+              required
+              multiple
+              accept="image/*, .png, .jpg, .jpeg, .pdf"
+              name="opd-to-class"
+              id="opd-to-class"
+              type="file"
+              onChange={hadleChangeFiles}
+            ></input>
+          </div>
+        </form>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <BackButton />
+          <button className="register__add-btn">Отправить</button>
         </div>
-        <BackButton />
       </div>
     </section>
   );
