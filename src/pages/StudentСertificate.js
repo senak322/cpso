b7d09bc6afd68fa8fa193376cd826b93b5f9dead
Aleksandr Viznyi <senak98@yandex.ru>
2023-03-27 18:40:31 +0400

    add certificate page

--- src/pages/StudentСertificate.js
+++ src/pages/StudentСertificate.js
@@ -1,7 +1,38 @@
+import { useParams } from "react-router-dom";
+import BackButton from "../components/BackButton";
+
 function StudentСertificate() {
-    return (
-<div>StudentСertificate</div>
-    )
+  let { classid } = useParams();
+  const studentEl = JSON.parse(localStorage.getItem("currentStudent"));
+  return (
+    <section className="register">
+      <div className="register__container">
+        <h1 className="register__add-header">{classid} класс</h1>
+
+        <h2 className="register__user-name">
+          {studentEl.lastname +
+            " " +
+            studentEl.firstname +
+            " " +
+            studentEl.middlename}
+        </h2>
+        <a href="#" target="_blank" className="register__certificate-link">
+            Справка о зачислении в школу
+        </a>
+        <p className="register__certificate-discription">
+        Аттестация за выбранный класс доступна.
+Войдите на платформу
+        </p>
+        <a href="#" target="_blank" className="register__certificate-link">
+            Справка с оценками
+        </a>
+        <div className="mt-5" style={{ display: "flex", alignItems: "center" }}>
+          <BackButton />
+          <button className="register__add-btn">Запросить оригинал</button>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default StudentСertificate;
